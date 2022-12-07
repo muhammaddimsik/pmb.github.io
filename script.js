ce7afec559b5eaa66bdcbba03d09a1b6578d8d14
Muhammad Dimsik <119264442+muhammaddimsik@users.noreply.github.com>
2022-12-07 17:53:19 +0700

    membuat modal untuk konfirmasi pendaftaran

--- script.js
+++ script.js
@@ -16,14 +16,19 @@ dropdown.addEventListener("click", () => {
   item.classList.toggle("open-item");
 });
 
+// get button swap overlay
+const swap = document.querySelector(".swap-overlay");
+
 // Get the modal
 const modal = document.getElementById("myModal");
+const modalTwo = document.querySelector(".modal-1");
 
 // Get the button that opens the modal
 const btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 const tutup = document.querySelector(".tutup");
+const tutupTwo = document.querySelector(".tutup-2");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
@@ -35,9 +40,21 @@ tutup.onclick = function () {
   modal.style.display = "none";
 };
 
+tutupTwo.onclick = function () {
+  modalTwo.style.display = "none";
+  modal.style.display = "none";
+};
+
+swap.onclick = function () {
+  modalTwo.style.display = "block";
+};
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  } else if (event.target == modalTwo) {
+    modal.style.display = "none";
+    modalTwo.style.display = "none";
   }
 };
