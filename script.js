10b451f584e567697ce301da9d0850b766b9f70f
Muhammad Dimsik <119264442+muhammaddimsik@users.noreply.github.com>
2022-12-07 14:07:10 +0700

    membuat modal pada halaman daftar

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
 const nav = document.querySelector("nav");
-const body = document.querySelector("body");
 
 menu.addEventListener("click", () => {
   nav.classList.add("open-nav");
@@ -16,3 +15,29 @@ const item = document.querySelector(".menu-dropdown");
 dropdown.addEventListener("click", () => {
   item.classList.toggle("open-item");
 });
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const tutup = document.querySelector(".tutup");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+tutup.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
