312426f0ee9c4633c4bfa0a8de1536245df8cc5e
Muhammad Dimsik <119264442+muhammaddimsik@users.noreply.github.com>
2022-12-10 12:04:32 +0700

    membuat slider img pada mobile dan membuat alur pendaftaran pada halaman beranda

--- script.js
+++ script.js
@@ -30,24 +30,24 @@ const btn = document.getElementById("myBtn");
 const tutup = document.querySelector(".tutup");
 const tutupTwo = document.querySelector(".tutup-2");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
+// // When the user clicks on the button, open the modal
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// };
 
-// When the user clicks on <span> (x), close the modal
-tutup.onclick = function () {
-  modal.style.display = "none";
-};
+// // When the user clicks on <span> (x), close the modal
+// tutup.onclick = function () {
+//   modal.style.display = "none";
+// };
 
-tutupTwo.onclick = function () {
-  modalTwo.style.display = "none";
-  modal.style.display = "none";
-};
+// tutupTwo.onclick = function () {
+//   modalTwo.style.display = "none";
+//   modal.style.display = "none";
+// };
 
-swap.onclick = function () {
-  modalTwo.style.display = "block";
-};
+// swap.onclick = function () {
+//   modalTwo.style.display = "block";
+// };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
@@ -58,3 +58,37 @@ window.onclick = function (event) {
     modalTwo.style.display = "none";
   }
 };
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+  console.log(dots);
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
